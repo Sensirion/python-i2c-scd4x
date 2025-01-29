10e535521355f5c156f62a50b65384162b43e726
Driver Generator 2 <drivergenerator2@sensirion.com>
2025-01-29 12:04:35 +0000

    Generate SCD4x driver from SCD4x model version 2.0

--- sensirion_i2c_scd4x/commands.py
+++ sensirion_i2c_scd4x/commands.py
@@ -7,7 +7,7 @@
 #
 # Generator:     sensirion-driver-generator 1.1.2
 # Product:       scd4x
-# Model-Version: 1.0
+# Model-Version: 2.0
 #
 """
 The transfer classes specify the data that is transferred between host and sensor. The generated transfer classes
--- tests/test_scd4x.py
+++ tests/test_scd4x.py
@@ -7,7 +7,7 @@
 #
 # Generator:     sensirion-driver-generator 1.1.2
 # Product:       scd4x
-# Model-Version: 1.0
+# Model-Version: 2.0
 #
 
 import pytest
